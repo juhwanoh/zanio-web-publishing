a5455f82852b160465a1e32a92b1c6c5e258550f
zani <ohjuhwan@gmail.com>
2023-10-20 16:58:25 +0900

    카카오맵 웹 페이지 작성중

---
+++ kakao/kakao_map.js
@@ -0,0 +1,33 @@
+function createMap() {
+    
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+    // 지도 타입 컨트롤을 지도에 표시합니다
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+    // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+    return map;
+}
+
+function createMarker(lat, lng) {
+    // 마커가 표시될 위치입니다
+    var markerPosition = new kakao.maps.LatLng(lat, lng);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+
+    return marker;
+}
